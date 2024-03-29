283c846338dec9035886540b60759cfb0b4521f5
Teamofeyy <128955381+Teamofeyy@users.noreply.github.com>
2024-03-29 20:27:02 +0300

    Fixed error in obtaining user token

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export default (req, res, next) =>{
-    const token = req.headers.authorizations;
-
-    console.log(token);
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        const token = req.headers.authorization.split(' ')[1];
+        res.send(token);
+    } else {
+        res.status(401).json({ message: 'Authorization token missing or invalid' });
+    }
 };
