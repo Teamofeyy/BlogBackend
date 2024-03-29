99847f8925eda331e74d2a04619320d439ae48c2
Teamofeyy <128955381+Teamofeyy@users.noreply.github.com>
2024-03-29 20:57:56 +0300

    refactor chechAuth method

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,10 +1,21 @@
 import jwt from "jsonwebtoken";
 
-export default (req, res, next) =>{
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        const token = req.headers.authorization.split(' ')[1];
-        res.send(token);
+export default (req, res, next) => {
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, 'secret123');
+
+            req.userId = decoded._id;
+            next();
+        } catch (e) {
+            return res.status(403).json({
+                message: 'Нет доступа',
+            });
+        }
     } else {
-        res.status(401).json({ message: 'Authorization token missing or invalid' });
+        return res.status(403).json({
+            message: 'Нет доступа',
+        });
     }
-};
+}
