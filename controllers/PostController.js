9b14b870389c34b71410e8b42ec040170355a69a
Teamofeyy <128955381+Teamofeyy@users.noreply.github.com>
2024-03-30 21:38:40 +0300

    added get one post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,10 +1,46 @@
 import PostModel from '../models/post.js';
 
-const getAll = async(req, res) =>{
+export const getAll = async(req, res) =>{
     try {
-        const posts = await PostModel.find();
+        const posts = await PostModel.find().populate('user').exec();
 
         res.json(posts);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статьи',
+        });
+    }
+}
+
+export const getOne = async(req, res) =>{
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndUpdate({
+            _id: postId,
+        }, {
+            $inc: { viewsCount: 1 },
+        }, {
+            returnDocument: 'after',
+        }, 
+            (err, doc) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).json({
+                        message: 'Не удалось получить статьи',
+                    });
+                }
+
+                if(!doc) {
+                    return res.status(404).json({
+                        message: 'Статья не найдена'
+                    });
+                }
+
+                res.json(doc);
+            },
+        );
     } catch (err) {
         console.log(err)
         res.status(500).json({
