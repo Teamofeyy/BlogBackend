13cefe12c3140430d41a09545177c94e7dd279e4
Teamofeyy <128955381+Teamofeyy@users.noreply.github.com>
2024-03-31 21:51:01 +0300

    for ai

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express, { request, response } from 'express';
 import multer from 'multer';
+import cors from 'cors';
 
 import mongoose from 'mongoose';
 
@@ -29,6 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads')); 
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login );
